5190a92979c1f7fa3f2e4bfc13608fb651593ce1
Ariel Rabinovich <airabinovich@gmail.com>
2015-11-01 13:10:24 -0300

    implementacion de las primeras funcionalidades del ensamblador. Distingue codigo de comentarios

--- ensambladorMIPS/src/Translator.py
+++ ensambladorMIPS/src/Translator.py
@@ -1,3 +1,4 @@
+# coding=UTF-8
 '''
 Created on 31 de oct. de 2015
 
@@ -14,20 +15,19 @@ class Translator(object):
     '''
     classdocs
     '''
-
+    
     def __init__(self):
         '''
         Constructor
         TODO define fields
         '''
+        self.field = 0;
         
-    def getCodeFromLine(self, line):
-        assert(isinstance(line, str))
-        code = split(line, ";\n")[0]
-        if code:    # not contains empty string
-            return code
-        else:
-            return None
+    def getCodeFromLine(self, text):
+        assert(isinstance(text, str))   #if text is not a string the method will fail and exit
+        code = [split(line, ";")[0].strip() for line in split(text,"\n")]   #splits the string into lines and then takes the part behind the first ; occurrence
+        code = [l for l in code if l]    #removes empty lines and only comment lines
+        return code
         
     def translate(self, code):
         assert(isinstance(code, str))
