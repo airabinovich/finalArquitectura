765c14b4087bfa4813f2423fb801b101c4b3d15e
Juan Arce <juanjo.arce7456@gmail.com>
2016-02-29 03:41:29 -0300

    agregadas modificaciones de Java para que muestre bien los datos erroneos

--- DebuggerArquitecturaJava/src/View/View.java
+++ DebuggerArquitecturaJava/src/View/View.java
@@ -91,7 +91,7 @@ public class View implements SubjectSend, ObserverReceive {
         generalPurposeRegisters = new ArrayList<Pair<Integer,JTextField>>();
         memoryRegisters = new ArrayList<Pair<Integer,JTextField>>();
         
-        for(int i = 1; i <= 5; i++){
+        for(int i = 0; i < 5; i++){
         	generalPurposeRegisters.add(new Pair<Integer,JTextField>(i,new JTextField()));
         	memoryRegisters.add(new Pair<Integer,JTextField>(i,new JTextField()));
         }
@@ -164,8 +164,8 @@ public class View implements SubjectSend, ObserverReceive {
         continueModeButton.addActionListener(new ActionListener(){
 			@Override
 			public void actionPerformed(ActionEvent arg0) {
-				continueModeButton.setEnabled(false);
-				stepModeButton.setEnabled(false);
+				//continueModeButton.setEnabled(false);
+//				stepModeButton.setEnabled(false);
 				nextStepButton.setEnabled(false);
 				command = 'c';
 				notifySend();
@@ -178,8 +178,8 @@ public class View implements SubjectSend, ObserverReceive {
         stepModeButton.addActionListener(new ActionListener(){
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				continueModeButton.setEnabled(false);
-				stepModeButton.setEnabled(false);
+//				continueModeButton.setEnabled(false);
+				//stepModeButton.setEnabled(false);
 				nextStepButton.setEnabled(true);
 				command = 's';
 				notifySend();
@@ -193,7 +193,6 @@ public class View implements SubjectSend, ObserverReceive {
         nextStepButton.addActionListener(new ActionListener(){
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				System.out.println(e.toString());
 				command = 'n';
 				notifySend();
 			}
@@ -241,11 +240,11 @@ public class View implements SubjectSend, ObserverReceive {
 					updateMemoryRegisters(data);
 				}
 			}
+			frame.repaint();
   		}catch(NullPointerException e){
 			System.out.println("LLegó un argumento null a updateReceive");
 			e.printStackTrace();
 		}
-		frame.repaint();
 	}
 	
 	private boolean updateProcessorRegisters(Pair<String,Integer> data){
@@ -269,8 +268,12 @@ public class View implements SubjectSend, ObserverReceive {
 				System.out.println( field + " no pertenece a la etapa " + stage);
 				return false;
 			}
-			// setea el valor recibido en el JTextField que corresponde al nombre del campo, dentro del registro que corresponde
-			pipelineRegistersData.get(stage).get(index).getSnd().setText(data.getSnd().toString());
+			if(data.getFst().contains("instructionOut")){
+				pipelineRegistersData.get(stage).get(index).getSnd().setText("0x"+Integer.toHexString(data.getSnd()).toUpperCase());
+			}else{
+				// setea el valor recibido en el JTextField que corresponde al nombre del campo, dentro del registro que corresponde
+				pipelineRegistersData.get(stage).get(index).getSnd().setText(Integer.toUnsignedString(data.getSnd()));
+			}
 		}catch(IndexOutOfBoundsException e){
 			return false;
 		}catch(NullPointerException e){
